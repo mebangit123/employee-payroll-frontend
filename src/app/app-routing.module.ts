25f764b4a9aa2f06007d7c0a9a97bd040bd110fc
mebangit123 <mebannongrum333@gmail.com>
2021-08-25 19:46:00 +0530

    added: header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
 
-
-const routes: Routes = [];
+/**
+ * Represents a route configuration for the Router service.
+ */
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
